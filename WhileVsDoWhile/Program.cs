91ae41334e7ca1dd7b52ec131bb8b73af2e9ce34
ericekirem <irem.ericek@icloud.com>
2024-08-14 00:43:37 +0300

    Add project files.

---
+++ WhileVsDoWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// While Döngüsü
+
+Console.Write("Bir limit değeri giriniz: ");
+int limit = int.Parse(Console.ReadLine());
+int counter = 0;
+
+while (counter <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    counter++;
+}
+
+
+
+
+
+// Do-While Döngüsü:
+
+Console.Write("Bir limit değeri giriniz: ");
+        int limit = int.Parse(Console.ReadLine());
+        int counter = 0;
+
+        do
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            counter++;
+        } while (counter <= limit);
+
+
+
+
+// While döngüsünde kod baştan kontrol edildiği için -5 yazıldığında hata veriyor.
+// Do-While döngüsünde ise şartı sağlamamasına rağmen döngü yine de bir kez "Ben bir Patika'lıyım" yazdırıyor.
+// İki döngüde de "10" değerini yazdığımda, ekrana 11 kez "Ben bir Patika'lıyım" yazdırıyor.
